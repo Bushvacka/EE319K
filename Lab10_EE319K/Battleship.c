8dc74a467bba3c3bed1f456e32896d428da53029
Bushvacka <45671229+Bushvacka@users.noreply.github.com>
2022-04-26 14:09:07 -0500

    Implemented starting sequence

--- Lab10_EE319K/Battleship.c
+++ Lab10_EE319K/Battleship.c
@@ -35,13 +35,31 @@
 #include "Timer1.h"
 #include "Fifo.h"
 #include "Uart.h"
+#include "SysTick.h"
 
-#define PORTE_PRIO 3
+#define PORTE_PRIO 4
+#define SYSTICK_PRIO 3
+#define TIMER1_PRIO 2
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 void Delay100ms(uint32_t count); // time delay in 0.1 seconds
 int PLL_Init(void);
+
+typedef enum {English, Spanish} Language_t;
+Language_t language = English;
+typedef enum {Start, Wait, LANGUAGE} phrase_t;
+const char Start_English[] ="Start";
+const char Start_Spanish[] ="Comienzo";
+const char Wait_English[]="Waiting for Player";
+const char Wait_Spanish[]="Esperando al Jugador";
+const char Language_English[]="English";
+const char Language_Spanish[]="Espa\xA4ol";
+const char *Phrases[3][2]={
+  {Start_English,Start_Spanish},
+  {Wait_English,Wait_Spanish},
+  {Language_English,Language_Spanish}
+};
 
 // Initialize PortF for heartbeat
 void PortF_Init(void){
@@ -53,7 +71,7 @@ void PortF_Init(void){
 }
 
 // Initialize PortE for edge-triggered buttons
-void PortE_Init(void){
+void PortE_Init(uint32_t priority){
 	volatile uint32_t delay;
   SYSCTL_RCGCGPIO_R |= 0x10; // Port E
 	delay = SYSCTL_RCGCGPIO_R;
@@ -64,35 +82,33 @@ void PortE_Init(void){
   GPIO_PORTE_IEV_R |= 0xC;  // Rising edge trigger
   GPIO_PORTE_ICR_R = 0xC;   // Clear flag
   GPIO_PORTE_IM_R |= 0xC;   // Eanble interrupt
-  NVIC_PRI11_R = (NVIC_PRI11_R&0xFFFFFF1F)| (PORTE_PRIO << 5); // Priority 3
+  NVIC_PRI11_R = (NVIC_PRI11_R&0xFFFFFF1F) | (priority << 5); // Priority 3
   NVIC_EN0_R = 0x10;   			 // Enable IRQ 4
 }
 
-uint32_t ADC_Data;
-uint8_t ADC_Flag;
-// Initialize SysTick for sampling the ADC
-void SysTick_Init(uint32_t period){
-	NVIC_ST_CTRL_R = 0; // Disable SysTick during setup
-	NVIC_ST_CURRENT_R = 0; // Reset counter
-	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x0FFFFFFF)|0x40000000; // Set priority to 2
-	NVIC_ST_RELOAD_R = period - 1; // Load period
-	NVIC_ST_CTRL_R = 0x07; // Enable interrupts & core clock
-	ADC_Flag = 0;
-}
-
+uint8_t shoot;
+// Handle a button press (lock-in/shoot or change language)
 void GPIOPortE_Handler(void){
   GPIO_PORTE_ICR_R = 0xC; // Ack
-	GPIO_PORTF_DATA_R ^= 0x4; // Heartbeat
+	if ((GPIO_PORTE_DATA_R & 0x8) == 0x8) {
+		shoot = 1;
+	} else {
+		if (language == English) {
+			language = Spanish;
+		} else {
+			language = English;
+		}
+	}
 }
 
+uint8_t blinker = 0;
 void Timer1A_Handler(void){ // can be used to perform tasks in background
-  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// Ack
-}
-
-// Sample the ADC and alert the main program
-void SysTick_Handler(void) {
-	ADC_Data = ADC_In();
-	ADC_Flag = 1;
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT; // Ack
+	if (blinker) {
+		blinker = 0;
+	} else {
+		blinker = 1;
+	}
 }
 
 // Convert ADC input to a square
@@ -101,7 +117,6 @@ uint32_t Convert(uint32_t x){
 }
 
 void Delay100ms(uint32_t count){uint32_t volatile time;
-// Language setup - English default
   while(count>0){
     time = 727240;  // 0.1sec at 80 MHz
     while(time){
@@ -110,51 +125,56 @@ void Delay100ms(uint32_t count){uint32_t volatile time;
     count--;
   }
 }
-typedef enum {English, Spanish} Language_t;
-Language_t myLanguage = English;
-typedef enum {HELLO, GOODBYE, LANGUAGE} phrase_t;
-const char Hello_English[] ="Hello";
-const char Hello_Spanish[] ="\xADHola!";
-const char Goodbye_English[]="Goodbye";
-const char Goodbye_Spanish[]="Adi\xA2s";
-const char Language_English[]="English";
-const char Language_Spanish[]="Espa\xA4ol";
-const char *Phrases[3][2]={
-  {Hello_English,Hello_Spanish},
-  {Goodbye_English,Goodbye_Spanish},
-  {Language_English,Language_Spanish}
-};
+
+// Display title sequence until the shoot button is pressed
+void startingSequence(void) {
+	
+	ST7735_FillScreen(ST7735_BLACK);
+	ST7735_DrawBitmap(13, 119, title, 39, 79);
+	while(shoot == 0) { // Wait for button press
+			if (blinker) {
+				ST7735_SetCursor(0, 0);
+				ST7735_OutString((char *)Phrases[Start][language]);
+			} else {
+				ST7735_SetCursor(0, 0);
+				ST7735_OutString("        ");
+			}
+	}
+	shoot = 0; // Ignore
+	UART_OutChar('R'); // Ready
+	char data = 0;
+	char *datapt = &data;
+	while(data != 'R') { 
+		Fifo_Get(datapt); 
+		if (blinker) {
+			ST7735_SetCursor(0, 0);
+			ST7735_OutString((char *)Phrases[Wait][language]);
+		} else {
+			ST7735_SetCursor(0, 0);
+			ST7735_OutString("        ");
+		}
+	} // Wait for ready signal
+	TIMER1_CTL_R = 0x0; // Disable Timer1
+}
 
 int main(void){
   DisableInterrupts();
 	PLL_Init();
   //TExaS_Init(NONE); // Bus clock is 80 MHz 
-	PortE_Init();
+	PortE_Init(PORTE_PRIO);
 	PortF_Init();
-  Output_Init();
+  ST7735_InitR(INITR_REDTAB);
+	Timer1_Init(80000000/10, TIMER1_PRIO);
 	ADC_Init();
 	Fifo_Init();
 	UART_Init();
 	Sound_Init();
-	SysTick_Init(80000000/30);
+	SysTick_Init(80000000/30, SYSTICK_PRIO);
 	Random_Init(NVIC_ST_CURRENT_R);
 	EnableInterrupts();
-  ST7735_FillScreen(0x0000); // Black
-  for(phrase_t myPhrase=HELLO; myPhrase<= GOODBYE; myPhrase++){
-    for(Language_t myL=English; myL<= Spanish; myL++){
-         ST7735_OutString((char *)Phrases[LANGUAGE][myL]);
-      ST7735_OutChar(' ');
-         ST7735_OutString((char *)Phrases[myPhrase][myL]);
-      ST7735_OutChar(13);
-    }
-  }
-	ST7735_FillScreen(0x0000); // Black
+	startingSequence();
 	while (1) {
-		if (ADC_Flag) {
-			ST7735_SetCursor(0, 0);
-			ADC_Flag = 0;
-			LCD_OutFix(ADC_Data);
-		}
+		Delay100ms(5);
 	}
 }
 
