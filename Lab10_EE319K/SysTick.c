cba08b8ed2f8de63d46450cc7be2bd15ab7b0f91
Bushvacka <45671229+Bushvacka@users.noreply.github.com>
2022-04-27 21:57:39 -0500

    Samples PortE

--- Lab10_EE319K/SysTick.c
+++ Lab10_EE319K/SysTick.c
@@ -9,8 +9,9 @@
 #include "ADC.h"
 #include "../inc/tm4c123gh6pm.h"
 
-uint32_t ADC_Data;
-uint8_t ADC_Flag = 0;
+uint8_t button1;
+uint8_t button2;
+uint32_t previous_data;
 
 // Initialize SysTick for sampling the ADC
 void SysTick_Init(uint32_t period, uint32_t priority){
@@ -19,11 +20,25 @@ void SysTick_Init(uint32_t period, uint32_t priority){
 	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x0FFFFFFF)|(priority << 29); // Set priority
 	NVIC_ST_RELOAD_R = period - 1; // Load period
 	NVIC_ST_CTRL_R = 0x07; // Enable interrupts & core clock
-	ADC_Flag = 0;
+	//ADC_Flag = 0;
+	button1 = 0;
+	button2 = 0;
+	previous_data = 0xFFFFFFFF;
 }
 
 // Sample the ADC and alert the main program
 void SysTick_Handler(void) {
-	ADC_Data = ADC_In();
-	ADC_Flag = 1;
+	uint32_t data = GPIO_PORTE_DATA_R;
+	if ((data & 0x8) == 0x8 && (previous_data & 0x8) == 0) { // Button 1 - Fire/Lock in/Ready up
+	button1 = 1;
+	}
+	else if ((data & 0x4) == 0x4 && (previous_data & 0x4) == 0) { // Button 2 - Rotate ship
+	button2 = 1;
+	}
+	/*else if((data & 0x2) == 0x2 && (previous_data & 0x2) == 0) { // Button 3 - Language Change
+		if (language == English) {language = Spanish;} 
+		else {language = English;}
+	}*/
+	previous_data = data;
 }
+
